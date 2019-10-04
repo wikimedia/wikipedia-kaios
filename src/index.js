5c932ac5d6d5b26fc490c9b401a72d887a298fa1
Stephane Bisson <sbisson@wikimedia.org>
2019-10-04 14:13:27 -0400

    Basic i18n support with banana-i18n
    Bug: T234614

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-var root = document.querySelector('#root');
-root.innerText = 'Wikipedia';
+import Banana from 'banana-i18n';
+
+const lang = navigator.language.substr( 0, 2 );
+const banana = new Banana( lang );
+fetch( 'i18n/' + lang + '.json' )
+	.then( ( response ) => response.json() )
+	.then( ( messages ) => banana.load( messages, lang ) )
+	.then( () => {
+		const root = document.querySelector('#root');
+		root.innerText = banana.i18n( 'app-title' );
+	} );
