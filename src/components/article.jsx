0d1478ccc7ada499fbad5271177a88e38f41be05
Stephane Bisson <sbisson@wikimedia.org>
2019-10-25 20:21:26 -0400

    Make the article one long scrollable page

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -1,23 +1,12 @@
 import { h, Fragment } from 'preact'
-import { useState } from 'preact/hooks'
 import { ArticleHeader } from 'components/articleheader.jsx'
-import { ArticleBody } from 'components/articlebody.jsx'
 import { Softkey } from 'components/softkey'
 import { useArticle } from 'hooks/useArticle'
 import { useBackToSearch } from 'hooks/useBackToSearch'
-import { useKeys } from 'hooks/useKeys'
 
 export const Article = ({ lang, title }) => {
   const article = useArticle(lang, title)
-  const [showBody, setShowBody] = useState(false)
-
   useBackToSearch()
-
-  useKeys({
-    ArrowDown: () => {
-      setShowBody(true)
-    }
-  })
 
   if (!article) {
     return 'Loading...'
@@ -25,21 +14,16 @@ export const Article = ({ lang, title }) => {
 
   return (
     <Fragment>
-      { (showBody) ? (
-        <ArticleBody content={article.content} />
-      ) : (
-        <ArticleHeader
-          imageUrl={article.imageUrl}
-          title={article.title}
-          description={article.description}
-          preview={article.preview}
-        />
-      ) }
+      <ArticleHeader
+        imageUrl={article.imageUrl}
+        title={article.title}
+        description={article.description}
+        preview={article.preview}
+        content={article.content}
+      />
       <Softkey
         left='Settings'
       />
     </Fragment>
   )
 }
-
-// export default Article
--- src/components/articleheader.jsx
+++ src/components/articleheader.jsx
@@ -1,14 +1,22 @@
 import { h, Fragment } from 'preact'
+import { useRef } from 'preact/hooks'
+import { useScroll } from 'hooks/useScroll'
 
 export const ArticleHeader = ({
   imageUrl,
   title,
   description,
-  preview
+  preview,
+  content
 }) => {
   const hasImage = !!imageUrl
+  const contentRef = useRef()
+  const step = 20
+
+  useScroll(contentRef, step)
+
   return (
-    <div class='page article-header'>
+    <div class='page article-header' ref={contentRef}>
       { hasImage ? <img src={imageUrl} /> : '' }
       <div class={'card' + (hasImage ? ' with-image' : '')}>
         <div class='title' dangerouslySetInnerHTML={{ __html: title }} />
@@ -18,7 +26,10 @@ export const ArticleHeader = ({
             <div class='line' />
           </Fragment>
         ) : '' }
-        { hasImage ? '' : <div class='preview' dangerouslySetInnerHTML={{ __html: preview }} /> }
+        <div
+          class='article-content'
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
       </div>
     </div>
   )
