317d41703a33f2c2a359e286e87284a7c541a17d
Jose Pita <dvpita@gmail.com>
2019-11-08 00:10:12 +0800

    first test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,11 +1,40 @@
 /// <reference types="Cypress" />
-// @ts-check
 
-it('loads', () => {
-  cy.visit('localhost:3000')
-  cy.contains('h1', 'todos')
-  cy.screenshot({
-    capture: 'runner'
+import { SearchPage } from "../page-objects/search-page"
+
+// @ts-check
+const searchPage = new SearchPage()
+describe('Article search', () =>{
+  
+
+  beforeEach(() =>{
+    searchPage.navigateToSearchPage()
+    searchPage.search("catt")
   })
 
+  it('search should show results', () => {
+    cy.get('.result').first()
+      .should('have.text', "Cattaraugus County, New YorkCounty in New York")
+  })
+
+  it('results with image should show image', () => {
+    cy.get('.result').first()
+      .children().should('have.length', 2)
+  })
+
+  it('results without image should not show image', () => {
+    cy.get('.result').first().next()
+      .children().should('have.length', 1)
+  })
+
+  
+})
+it.only('article should open from search results page', () => {
+  searchPage.navigateToSearchPage()
+  cy.get('input[type=text]').type("catt")
+  cy.get('.result').first().next()
+      .children().should('have.length', 1)
+  cy.get('body').type('{enter}').type('{downarrow}').type('{enter}')
+  cy.get('.title').should('have.text', "Cattaraugus County, New York")
+//cy.get('body').type('{uparrow}{uparrow}{downarrow}{downarrow}{leftarrow}{rightarrow}{leftarrow}{rightarrow}')
 })
