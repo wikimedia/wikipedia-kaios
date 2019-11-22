1c8cbcc1ce1d0143b9ffcf8f045f76a2430bf046
Huei Tan <huei90@gmail.com>
2019-11-22 11:04:13 +0100

    Add <ListView/> Component

---
+++ src/components/ListView.js
@@ -0,0 +1,23 @@
+import { h } from 'preact'
+
+/**
+ * The containerRef is suggested to be used together with the hooks useNavigation
+ * without the containerRef, the view won't scroll to the selected row
+ */
+export const ListView = ({ items, containerRef }) => {
+  if (!items.length) return
+  return <div class='results' ref={containerRef}>
+    {
+      items.map(item => (
+        <div class='result' data-selectable data-title={item.title} data-selected-key={item.title} key={item.title}>
+          <div class='info'>
+            <div class='title' dangerouslySetInnerHTML={{ __html: item.titleHtml }} />
+            <div class='description'>{item.description}</div>
+          </div>
+          { item.imageUrl && <div class='img'><img src={item.imageUrl} /></div> }
+        </div>
+
+      ))
+    }
+  </div>
+}
