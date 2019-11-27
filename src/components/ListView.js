9c8d36cb67b25c0d712abafb45e9b183c4105826
Andreas P <me@apap04.com>
2019-11-27 00:37:02 -0500

    Added links to Search
    Users can now go to links from the Search page. Currently,
    the language is set to `en`, but should be changed later on to
    reflect user settings.
    
    Signed-off-by: Andreas P <me@apap04.com>

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -10,10 +10,10 @@ export const ListView = ({ items, containerRef }) => {
     {
       items.map(item => (
         <div class='item' data-selectable data-title={item.title} data-selected-key={item.title} key={item.title}>
-          <div class='info'>
+          <a class='info' href={`/article/en/${item.title}`}>
             <div class='title' dangerouslySetInnerHTML={{ __html: item.titleHtml || item.title }} />
             { item.description && <div class='description'>{item.description}</div> }
-          </div>
+          </a>
           { item.imageUrl && <div class='img'><img src={item.imageUrl} /></div> }
           { item.link && <div class='link'><img src='/images/link.svg' /></div> }
         </div>
