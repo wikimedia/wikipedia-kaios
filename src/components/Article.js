3ba9ff34213ec1c3eac40ccc65809278da5f3148
Stephane Bisson <sbisson@wikimedia.org>
2019-12-03 11:49:17 -0500

    Extract ArticleSection for readability

--- src/components/Article.js
+++ src/components/Article.js
@@ -13,33 +13,27 @@ const ArticleBody = memo(({ content }) => {
   )
 })
 
-export const Article = ({ lang, title }) => {
+const ArticleSection = ({
+  lang, imageUrl, title, description, hasActions, content
+}) => {
   const i18n = useI18n()
-  const article = useArticle(lang, title)
+  const softkey = useSoftkey()
   const actionsRef = useRef()
   const [, setNavigation, getCurrent] = useNavigation(actionsRef, 'x')
-  const softkey = useSoftkey()
-  const [currentSection, setCurrentSection] = useState(0)
-  const imageUrl = article && article.sections[currentSection].imageUrl
-
-  if (!article) {
-    return 'Loading...'
-  }
-
-  useEffect(() => {
-    softkey.dispatch({ type: 'setLeftText', value: i18n.i18n('close') })
-    softkey.dispatch({ type: 'setCenterText', value: i18n.i18n('centerkey-select') })
-    softkey.dispatch({ type: 'setOnKeyLeft', event: onKeyLeft })
-    softkey.dispatch({ type: 'setOnKeyCenter', event: onKeyCenter })
-  }, [])
 
   useEffect(() => {
     setNavigation(0)
-  }, [article])
+  }, [])
 
-  const onKeyLeft = () => {
-    history.back()
-  }
+  useEffect(() => {
+    // todo: the enter key should only be available on the
+    // first page of the first section
+    const text = hasActions ? i18n.i18n('centerkey-select') : ''
+    const handler = hasActions ? onKeyCenter : null
+    softkey.dispatch({ type: 'setCenterText', value: text })
+    softkey.dispatch({ type: 'setOnKeyCenter', event: handler })
+  }, [hasActions])
+
   const onKeyCenter = () => {
     const current = getCurrent()
     if (current.key === 'quickfacts') {
@@ -48,6 +42,60 @@ export const Article = ({ lang, title }) => {
   }
   const goToQuickFacts = () => {
     window.location.hash = `/quickfacts/${lang}/${title}`
+  }
+
+  return (
+    <Fragment>
+      { imageUrl && <div class='lead-image' style={{ backgroundImage: `url(${imageUrl})` }} /> }
+      <div class={'card' + (imageUrl ? ' with-image' : '')}>
+        <div class='title' dangerouslySetInnerHTML={{ __html: title }} />
+        { description && (
+          <Fragment>
+            <div class='desc'>{description}</div>
+            <div class='line' />
+          </Fragment>
+        ) }
+        { hasActions && (
+          <div class='article-actions' ref={actionsRef}>
+            <div class='article-actions-button' data-selectable data-selected-key='sections'>
+              <img src='images/sections.svg' />
+              <label>Sections</label>
+            </div>
+            <div class='article-actions-button' data-selectable data-selected-key='quickfacts'>
+              <img src='images/quickfacts.svg' />
+              <label>Quick Facts</label>
+            </div>
+            <div class='article-actions-button' data-selectable data-selected-key='audio'>
+              <img src='images/audio.svg' />
+              <label>Audio</label>
+            </div>
+          </div>
+        ) }
+        <ArticleBody content={content} />
+      </div>
+    </Fragment>
+  )
+}
+
+export const Article = ({ lang, title }) => {
+  const i18n = useI18n()
+  const article = useArticle(lang, title)
+  const softkey = useSoftkey()
+  const [currentSection, setCurrentSection] = useState(0)
+
+  if (!article) {
+    return 'Loading...'
+  }
+
+  const section = article.sections[currentSection]
+
+  useEffect(() => {
+    softkey.dispatch({ type: 'setLeftText', value: i18n.i18n('close') })
+    softkey.dispatch({ type: 'setOnKeyLeft', event: onKeyLeft })
+  }, [])
+
+  const onKeyLeft = () => {
+    history.back()
   }
 
   const pagerEvent = {
@@ -66,38 +114,17 @@ export const Article = ({ lang, title }) => {
   }
 
   return (
-    <Fragment>
-      <Pager event={pagerEvent}>
-        <div class='page article'>
-          { imageUrl && <div class='lead-image' style={{ backgroundImage: `url(${imageUrl})` }} /> }
-          <div class={'card' + (imageUrl ? ' with-image' : '')}>
-            <div class='title' dangerouslySetInnerHTML={{ __html: article.sections[currentSection].title }} />
-            { article.sections[currentSection].description && (
-              <Fragment>
-                <div class='desc'>{article.sections[currentSection].description}</div>
-                <div class='line' />
-              </Fragment>
-            ) }
-            { currentSection === 0 && (
-              <div class='article-actions' ref={actionsRef}>
-                <div class='article-actions-button' data-selectable data-selected-key='sections'>
-                  <img src='images/sections.svg' />
-                  <label>Sections</label>
-                </div>
-                <div class='article-actions-button' data-selectable data-selected-key='quickfacts'>
-                  <img src='images/quickfacts.svg' />
-                  <label>Quick Facts</label>
-                </div>
-                <div class='article-actions-button' data-selectable data-selected-key='audio'>
-                  <img src='images/audio.svg' />
-                  <label>Audio</label>
-                </div>
-              </div>
-            ) }
-            <ArticleBody content={article.sections[currentSection].content} />
-          </div>
-        </div>
-      </Pager>
-    </Fragment>
+    <Pager event={pagerEvent}>
+      <div class='page article'>
+        <ArticleSection
+          lang={lang}
+          title={section.title}
+          description={section.description}
+          imageUrl={section.imageUrl}
+          hasActions={currentSection === 0}
+          content={section.content}
+        />
+      </div>
+    </Pager>
   )
 }
