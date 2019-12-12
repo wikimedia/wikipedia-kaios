54c96fc85b2653e002c1ecd000cde46ba65c7bd7
Stephane Bisson <sbisson@wikimedia.org>
2019-12-12 10:44:30 -0500

    Cleanup

--- src/hooks/useSoftkey.js
+++ src/hooks/useSoftkey.js
@@ -3,11 +3,7 @@ import { SoftkeyContext } from 'contexts'
 
 export const useSoftkey = (origin, config = null, dependencies = []) => {
   const softkey = useContext(SoftkeyContext)
-  // todo: push the stack concept all the way down to useKeys
-  // so that other handlers (useNavigation) don't work while
-  // a popup is open
   useEffect(() => {
-    console.log('useSoftkey', origin)
     softkey.dispatch({ type: 'push', origin })
     return () => { softkey.dispatch({ type: 'pop', origin }) }
   }, [origin])
