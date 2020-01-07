ec8b9ac5d6bdb0532676a489b77932f5cbbbdb49
Jose Pita <dvpita@gmail.com>
2020-01-07 21:53:52 +0100

    use translation json files to get strings for tests

--- cypress/integration/settings-spec.js
+++ cypress/integration/settings-spec.js
@@ -1,28 +1,45 @@
 /// <reference types="Cypress" />
 
-
-import { SettingsPage } from "../page-objects/settings-page"
-import { SearchPage } from "../page-objects/search-page"
-import { LanguageSettingsPage } from "../page-objects/language-settings-page"
+import * as enJson from '../../i18n/en.json'
+import * as nlJson from '../../i18n/nl.json'
+import { SettingsPage } from '../page-objects/settings-page'
+import { SearchPage } from '../page-objects/search-page'
+import { LanguageSettingsPage } from '../page-objects/language-settings-page'
 
 const searchPage = new SearchPage()
 const settingsPage = new SettingsPage()
 const languageSettingsPage = new LanguageSettingsPage()
+const settingsMenuListEnglishText = [enJson['settings-language'],
+  enJson['settings-textsize'],
+  enJson['settings-about-wikipedia'],
+  enJson['settings-privacy'],
+  enJson['settings-term'],
+  enJson['settings-rate'],
+  enJson['settings-help-feedback'],
+  enJson['settings-about-app']]
+const languageSettingsPopupEnglishText = enJson['language-setting-message']
+const languageChangeDutchText = nlJson['language-change']
+const settingsMenuListDutchText = [nlJson['settings-language'],
+  nlJson['settings-textsize'],
+  nlJson['settings-about-wikipedia'],
+  nlJson['settings-privacy'],
+  nlJson['settings-term'],
+  nlJson['settings-rate'],
+  nlJson['settings-help-feedback'],
+  nlJson['settings-about-app']]
 
-
-describe('settings page', () =>{
+describe('settings page', () => {
   var firstElementOfTheSettingsMenuList
-  beforeEach(() =>{
+  beforeEach(() => {
     cy.navigateToHomePage()
     searchPage.navigateToSettingsPage()
     firstElementOfTheSettingsMenuList = settingsPage.settingsList().children().first()
   })
 
   it('show all the items in the list', () => {
-    settingsPage.settingsList().should('have.text', 
-    "LanguageText sizeAbout WikipediaPrivacy policyTerms of useRate the appHelp and feedbackAbout the app")
+    settingsPage.settingsList().should('have.text', settingsMenuListEnglishText.toString().replace(/,/g, ''))
   })
-  
+
   it('down arrow changes selection', () => {
     firstElementOfTheSettingsMenuList.should('have.attr', 'nav-selected', 'true')
 
@@ -43,21 +60,19 @@ describe('settings page', () =>{
 
   it('language of the app should change', () => {
     cy.enter()
-    languageSettingsPage.popupTitleElement().should('have.text', 'Language settings')
+    languageSettingsPage.popupTitleElement().should('have.text', enJson['language-setting'])
 
-    languageSettingsPage.popupTextElement().should('have.text', 'Changing the language here will change the language throughout the app. You can also change the language on each article using the article menu.')
+    languageSettingsPage.popupTextElement().should('have.text', languageSettingsPopupEnglishText)
     cy.enter()
 
     cy.get('.left').should('have.text', 'Search')
-    
+
     cy.downArrow()
     cy.enter()
 
-    languageSettingsPage.headerElement().should('have.text', 'Programmataal wijzigen')
+    languageSettingsPage.headerElement().should('have.text', languageChangeDutchText)
 
     cy.get('.right').click()
-    settingsPage.settingsList().should('have.text', 
-    "TaalTekstgrootteOver WikipediaPrivacybeleidGebruiksvoorwaardenApp beoordelenHulp en feedbackOver de app")
+    settingsPage.settingsList().should('have.text', settingsMenuListDutchText.toString().replace(/,/g, ''))
   })
-
 })
