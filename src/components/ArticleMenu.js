10ecc5d490276ec313ddf010590ec000d0024ccd
Huei Tan <hueitan@users.noreply.github.com>
2020-01-10 16:37:53 +0100

    Translation of the article actions button text (#79)
    * Translation of the article actions button text
    
    * Cleanup qqq and en lang file
    
    * More details in article-action-* key
    
    https://phabricator.wikimedia.org/T234628#5792612

--- src/components/ArticleMenu.js
+++ src/components/ArticleMenu.js
@@ -41,7 +41,7 @@ export const ArticleMenu = ({ close, onTocSelected, onLanguageSelected }) => {
   }, [])
 
   const items = [
-    { title: i18n.i18n('menu-section'), action: onTocSelected },
+    { title: i18n.i18n('article-action-sections'), action: onTocSelected },
     { title: i18n.i18n('menu-textsize'), action: onTextsizeSelected },
     { title: i18n.i18n('menu-language'), action: onLanguageSelected }
   ]
