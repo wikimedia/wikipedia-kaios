222c07614e9fe17a7e102572a27926f4455cab41
Huei Tan <hueitan@users.noreply.github.com>
2020-01-15 05:05:56 +0800

    Empty State for Search and Article Language Page (#83)
    * Add loading text in article language page
    
    * Add empty state in ListView and RadioListView
    
    * Add empty message in search and article language page
    
    * Add no result found message in App Language Settings

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -4,24 +4,24 @@ import { h } from 'preact'
  * The containerRef is suggested to be used together with the hooks useNavigation
  * without the containerRef, the view won't scroll to the selected row
  */
-export const ListView = ({ items, header, containerRef }) => {
-  if (!items.length) return
-  return <div class='listview'>
-    { header && <div class='header'>{header}</div> }
-    <div class='list' ref={containerRef}>
-      {
-        items.map(item => (
-          <div class='item' data-selectable data-title={item.title} data-selected-key={item.title} key={item.title}>
-            <div class='info'>
-              <div class='title' dangerouslySetInnerHTML={{ __html: item.titleHtml || item.title }} />
-              { item.description && <div class='description'>{item.description}</div> }
+export const ListView = ({ items = [], header, containerRef, empty }) => {
+  return (
+    <div class='listview'>
+      { header && <div class='header'>{header}</div> }
+      <div class='list' ref={containerRef}>
+        {
+          items.length ? items.map(item => (
+            <div class='item' data-selectable data-title={item.title} data-selected-key={item.title} key={item.title}>
+              <div class='info'>
+                <div class='title' dangerouslySetInnerHTML={{ __html: item.titleHtml || item.title }} />
+                { item.description && <div class='description'>{item.description}</div> }
+              </div>
+              { item.imageUrl && <div class='img' style={{ backgroundImage: `url(${item.imageUrl})` }} /> }
+              { item.link && <div class='link'><img src='/images/link.svg' /></div> }
             </div>
-            { item.imageUrl && <div class='img' style={{ backgroundImage: `url(${item.imageUrl})` }} /> }
-            { item.link && <div class='link'><img src='/images/link.svg' /></div> }
-          </div>
-
-        ))
-      }
+          )) : <div class='empty'>{empty}</div>
+        }
+      </div>
     </div>
-  </div>
+  )
 }
