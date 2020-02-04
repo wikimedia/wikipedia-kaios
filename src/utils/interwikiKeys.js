faa16a938775db8881ca935ddd0562894cd7c49b
Huei Tan <hueitan@users.noreply.github.com>
2020-02-04 13:40:28 +0100

    Opening up the interwiki link instead of preview (#112)
    * Opening up the interwiki link instead of preview
    
    * check the link href is a valid http link
    
    * Update regex
    
    * use interwiki keys
    
    * remove extra empty line

---
+++ src/utils/interwikiKeys.js
@@ -0,0 +1,29 @@
+// Interwiki titles and shortcuts key can be found
+// https://en.wikipedia.org/wiki/Help:Interwiki_linking#Project_titles_and_shortcuts
+export const INTERWIKI_KEYS = [
+  'wikipedia', 'w',
+  'simple',
+  'wiktionary', 'wikt',
+  'wikinews', 'n',
+  'wikibooks', 'b',
+  'wikiquote', 'q',
+  'wikisource', 's',
+  'oldwikisource',
+  'wikispecies', 'species',
+  'wikiversity', 'v',
+  'wikivoyage', 'voy',
+  'wikidata', 'd',
+  'wikimedia',
+  'foundation', 'wmf',
+  'commons', 'c',
+  'metawikipedia', 'meta', 'm',
+  'incubator',
+  'strategy',
+  'mediawikiwiki', 'mw',
+  'mediazilla',
+  'bugzilla',
+  'phabricator', 'phab',
+  'testwiki',
+  'wikitech',
+  'toolforge'
+]
