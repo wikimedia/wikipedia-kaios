5ba59c212ff2a0cf37b491d5c6e82ad0aaf7f9f0
Jose Pita <dvpita@gmail.com>
2020-02-19 09:01:04 +0800

    fix CR comments

--- cypress/page-objects/article-page.js
+++ cypress/page-objects/article-page.js
@@ -42,12 +42,6 @@ export class ArticlePage {
     cy.enter()
   }
 
-  goToLanguageMenuFromArticleLandingPage () {
-    cy.rightArrow().rightArrow().rightArrow()
-    cy.get('[data-action="languages"][data-selected="true"]').should('exist')
-    cy.enter()
-  }
-
   selectOption (option) {
     cy
       .get('.article-actions-button')
