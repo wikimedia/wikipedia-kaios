63e483156ccee715f723d916cd50a92af75e015c
Jose Pita <dvpita@gmail.com>
2020-02-24 13:25:53 +0800

    use the language file on commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,31 +1,6 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 import 'cypress-localstorage-commands'
-
+import * as enJson from '../../i18n/en.json'
 [
   'downArrow',
   'upArrow',
@@ -52,19 +27,19 @@ Cypress.Commands.add('getCenterSoftkeyButton', () => {
 })
 
 Cypress.Commands.add('clickCloseButton', () => {
-  cy.getLeftSoftkeyButton().contains('Close').click()
+  cy.getLeftSoftkeyButton().contains(enJson['softkey-close']).click()
 })
 
 Cypress.Commands.add('clickSettingsButton', () => {
-  cy.getRightSoftkeyButton().contains('Settings').click()
+  cy.getRightSoftkeyButton().contains(enJson['softkey-settings']).click()
 })
 
 Cypress.Commands.add('clickMenuButton', () => {
-  cy.getRightSoftkeyButton().contains('Menu').click()
+  cy.getRightSoftkeyButton().contains(enJson['softkey-menu']).click()
 })
 
 Cypress.Commands.add('clickDoneButton', () => {
-  cy.getLeftSoftkeyButton().contains('Done').click()
+  cy.getLeftSoftkeyButton().contains(enJson['softkey-done']).click()
 })
 
 Cypress.Commands.add('navigateToHomePage', () => {
