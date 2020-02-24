b6677fe64043edda307b9ae5b6dcbd0e9fc5384b
Huei Tan <huei90@gmail.com>
2020-02-24 21:15:32 +0100

    Add Video Component

---
+++ src/components/Video.js
@@ -0,0 +1,78 @@
+import { h } from 'preact'
+import { useState, useRef } from 'preact/hooks'
+import { useSoftkey, useI18n, useVideoNavigation } from 'hooks'
+
+export const Video = ({ close, items = [] }) => {
+  const i18n = useI18n()
+  const contentRef = useRef()
+  const [isPaused, togglePause] = useState(false)
+  const [itemIndex, setItemIndex] = useState(0)
+
+  // todo: change to item list
+  const video = ['https://upload.wikimedia.org/wikipedia/commons/2/23/2019-05-12_decouv-etape7-TdF2019-Belfort-576p.webm']
+
+  const linkHandlers = {
+    action: ({ action }) => {
+      const videoRef = contentRef.current.querySelector('video')
+      let newIndex
+      switch (action) {
+        case 'previous' :
+          newIndex = itemIndex - 1
+          setItemIndex(newIndex >= 0 ? newIndex : 0)
+          break
+        case 'backforward' :
+          videoRef.currentTime -= 10
+          break
+        case 'toggle' :
+          if (isPaused) {
+            videoRef.play()
+          } else {
+            videoRef.pause()
+          }
+          togglePause(!isPaused)
+          break
+        case 'fastforward' :
+          videoRef.currentTime += 10
+          break
+        case 'next' :
+          newIndex = itemIndex + 1
+          setItemIndex(newIndex < video.length ? newIndex : video.length - 1)
+          break
+      }
+    }
+  }
+  useVideoNavigation('Video', contentRef, linkHandlers, [isPaused])
+  useSoftkey('Video', {
+    left: i18n.i18n('softkey-close'),
+    onKeyLeft: close
+  })
+
+  return (
+    <div class='video' ref={contentRef}>
+      <p class='header'>Video of the day</p>
+      <div class='source'>
+        <video key={video[itemIndex]} autoplay='true' loop='true' controls='true' style='width:100%'>
+          <source src={video[itemIndex]} type='video/webm' />
+        Sorry, your browser doesn't support embedded videos.
+        </video>
+      </div>
+      <div class='panel'>
+        <div class='button' data-action='previous' key='previous'>
+          <img src='/images/video-previous.png' />
+        </div>
+        <div class='button forward' data-action='backforward' key='backforward'>
+          <img src='/images/video-backforward.png' />
+        </div>
+        <div class='button' data-action='toggle' key='toggle'>
+          <img src={`/images/video-${isPaused ? 'play' : 'pause'}.png`} />
+        </div>
+        <div class='button forward' data-action='fastforward' key='fastforward'>
+          <img src='/images/video-fastforward.png' />
+        </div>
+        <div class='button rotate' data-action='next' key='next'>
+          <img src='/images/video-previous.png' />
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/hooks/useVideoNavigation.js
@@ -0,0 +1,121 @@
+import { useState, useEffect } from 'preact/hooks'
+import { useSoftkey, useI18n } from 'hooks'
+import { viewport } from 'utils'
+
+// todo: refactoring
+// this is a copy of useArticleLinksNavigation,
+// try extract and find the general solution for the navigation purposed
+
+const SELECTED_ATTRIBUTE = 'data-selected'
+
+const SUPPORTED_LINKS = [
+  '[data-action]'
+].join(',')
+
+export const useVideoNavigation = (
+  origin,
+  contentRef,
+  linkHandlers = {},
+  dependencies = []
+) => {
+  const i18n = useI18n()
+  const [links, setLinks] = useState([])
+  const [currentLink, setCurrentLinkInternal] = useState(null)
+
+  const setCurrentLink = newLink => {
+    setCurrentLinkInternal(previousLink => {
+      if (previousLink) {
+        previousLink.removeAttribute(SELECTED_ATTRIBUTE)
+      }
+      if (newLink) {
+        newLink.setAttribute(SELECTED_ATTRIBUTE, true)
+      }
+      return newLink
+    })
+  }
+
+  const hasLinks = () => links && links.length
+
+  useEffect(() => {
+    const visibleLinks = findVisibleLinks(contentRef.current)
+    setLinks(visibleLinks)
+    if (visibleLinks.length) {
+      if (visibleLinks.indexOf(currentLink) === -1) {
+        setCurrentLink(visibleLinks[0])
+      }
+    } else {
+      setCurrentLink(null)
+    }
+  }, dependencies)
+
+  const defaultLinkHandlers = {}
+
+  const actualLinkHandlers = Object.assign({},
+    defaultLinkHandlers, linkHandlers)
+
+  useSoftkey(origin, {
+    onKeyArrowLeft: () => {
+      if (!hasLinks()) {
+        return
+      }
+      let i = links.indexOf(currentLink) - 1
+      if (i < 0) {
+        i = links.length - 1
+      }
+      setCurrentLink(links[i])
+    },
+    onKeyArrowRight: () => {
+      if (!hasLinks()) {
+        return
+      }
+      const i = (links.indexOf(currentLink) + 1) % links.length
+      setCurrentLink(links[i])
+    },
+    center: currentLink ? i18n.i18n('centerkey-select') : '',
+    onKeyCenter: () => {
+      if (!currentLink) {
+        return
+      }
+      const clickEvent = makeLinkClickEvent(currentLink)
+      const handler = actualLinkHandlers[clickEvent.type]
+      if (handler) {
+        handler(clickEvent)
+      }
+    }
+  }, [links, currentLink])
+
+  return [currentLink]
+}
+
+const makeLinkClickEvent = link => {
+  const action = link.getAttribute('data-action')
+  if (action) {
+    return { type: 'action', action }
+  }
+}
+
+const findVisibleLinks = container => {
+  const links = container.querySelectorAll(SUPPORTED_LINKS)
+  const visibleLinks = []
+  let rect
+  for (const link of links) {
+    rect = link.getBoundingClientRect()
+    if (!rect.height || !rect.width) {
+      // Hidden (probably display:none;)
+      continue
+    }
+    if (rect.x < 0 && (rect.x + rect.width < 0)) {
+      // Not visible on this page at all
+      continue
+    }
+    if (rect.y < 0 && (rect.y + rect.height < 0)) {
+      continue
+    }
+    if (rect.x > viewport.width || rect.y > (viewport.height - 30)) {
+      // After the current page
+      break
+    }
+    visibleLinks.push(link)
+  }
+  return visibleLinks
+}
