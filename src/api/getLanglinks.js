81f9c4a214e2ec976d21a89e20978c86930462bb
Eduardo <medina.eag@gmail.com>
2020-02-26 11:21:21 -0600

    Request native language name in getLanglinks

--- src/api/getLanglinks.js
+++ src/api/getLanglinks.js
@@ -6,14 +6,14 @@ export const getLanglinks = (lang, title) => {
     titles: title,
     prop: 'langlinks',
     lllimit: 500,
-    llprop: 'langname'
+    llprop: 'autonym'
   }
   const url = buildMwApiUrl(lang, params)
   return cachedFetch(url, response => {
     const { pages } = response.query
     return pages[0].langlinks.map(item => (
       {
-        title: item.langname,
+        title: item.autonym,
         lang: item.lang,
         description: item.title
       }
