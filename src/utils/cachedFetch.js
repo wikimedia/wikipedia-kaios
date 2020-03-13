24828436a4adf9a5b7e4a73a3e2628be2b057ce5
Stephane Bisson <sbisson@wikimedia.org>
2020-03-13 14:10:12 -0400

    Wrap mode check in utils

--- src/utils/cachedFetch.js
+++ src/utils/cachedFetch.js
@@ -1,3 +1,5 @@
+import { isProd } from 'utils'
+
 // todo: Implement a real cache that keeps
 // the last N requests to keep memory usage
 // under control.
@@ -19,7 +21,7 @@ export const cachedFetch = (url, transformFn, abortAllXhr = false, cache = true)
     xhrList[timestamp] = xhr
     xhr.responseType = 'json'
     xhr.open('GET', url)
-    if (process.env.NODE_ENV === 'production') {
+    if (isProd()) {
       // eslint-disable-next-line no-undef
       xhr.setRequestHeader('User-Agent', `WikipediaApp/${APP_VERSION} ${navigator.userAgent}`)
     }
