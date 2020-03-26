a62c95a5b83d56f28b40ad8765920651a30e36df
Huei Tan <huei90@gmail.com>
2020-03-26 15:59:50 +0100

    Fix media list api

--- src/api/getArticleMedia.js
+++ src/api/getArticleMedia.js
@@ -1,7 +1,7 @@
 import { cachedFetch, buildPcsUrl, canonicalizeTitle } from 'utils'
 
 export const getArticleMedia = (lang, title) => {
-  const url = buildPcsUrl(lang, title, 'media')
+  const url = buildPcsUrl(lang, title, 'media-list')
   return cachedFetch(url, data => data.items.reduce((mediaArray, item) => {
     if (item.showInGallery) {
       const media = {
@@ -13,7 +13,9 @@ export const getArticleMedia = (lang, title) => {
         thumbnail:
           (item.thumbnail && item.thumbnail.source) ||
           (item.srcset && item.srcset[0] && item.srcset[0].src),
-        canonicalizedTitle: item.titles && canonicalizeTitle(item.titles.normalized)
+        canonicalizedTitle:
+        (item.titles && canonicalizeTitle(item.titles.normalized)) ||
+        (item.title && canonicalizeTitle(item.title))
       }
 
       return mediaArray.concat(media)
