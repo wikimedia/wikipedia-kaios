e85950c37ac037f622016e05f3b086f45c447d5d
Huei Tan <huei90@gmail.com>
2020-03-26 16:15:47 +0100

    get the file name

--- src/api/getArticleMedia.js
+++ src/api/getArticleMedia.js
@@ -5,17 +5,16 @@ export const getArticleMedia = (lang, title) => {
   return cachedFetch(url, data => data.items.reduce((mediaArray, item) => {
     if (item.showInGallery) {
       const media = {
-        author: item.artist && item.artist.text,
-        caption: item.caption && item.caption.text,
-        description: item.description && item.description.text,
-        license: item.license && item.license.type,
-        filePage: item.file_page,
+        // author: item.artist && item.artist.text,
+        // caption: item.caption && item.caption.text,
+        // description: item.description && item.description.text,
+        // license: item.license && item.license.type,
+        // filePage: item.file_page,
         thumbnail:
           (item.thumbnail && item.thumbnail.source) ||
           (item.srcset && item.srcset[0] && item.srcset[0].src),
         canonicalizedTitle:
-        (item.titles && canonicalizeTitle(item.titles.normalized)) ||
-        (item.title && canonicalizeTitle(item.title))
+          item.title && canonicalizeTitle(item.title.split(':')[1])
       }
 
       return mediaArray.concat(media)
