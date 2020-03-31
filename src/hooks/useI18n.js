caa953a7f27a135d9b3cbfa777a8f074a0f5b3ee
Huei Tan <huei90@gmail.com>
2020-03-31 19:56:53 +0200

    Update code review

--- src/hooks/useI18n.js
+++ src/hooks/useI18n.js
@@ -4,16 +4,22 @@ import { getAppLanguage } from 'utils'
 
 export const useI18n = lang => {
   const banana = useContext(I18nContext)
-  banana.setLocale(getAppLanguage())
 
   return (key, ...args) => {
     if (lang) {
       const locale = banana.locale
       banana.setLocale(lang)
-      const msg = banana.i18n(key, args)
+      const msg = banana.i18n(key, ...args)
       banana.setLocale(locale)
       return msg
     }
+
+    // the script executes this line only
+    // when changing the app interface langauge
+    if (banana.locale !== getAppLanguage()) {
+      banana.setLocale(getAppLanguage())
+    }
+
     return banana.i18n(key, ...args)
   }
 }
