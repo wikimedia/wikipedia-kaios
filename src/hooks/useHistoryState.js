cef78508a0cc108ae06042e72f321139ed7d0dc3
Huei Tan <huei90@gmail.com>
2020-04-27 10:16:43 +0200

    fix

--- src/hooks/useHistoryState.js
+++ src/hooks/useHistoryState.js
@@ -2,7 +2,7 @@ import { useState } from 'preact/hooks'
 
 export const useHistoryState = (key, initialValue) => {
   const [current, setCurrent] = useState(() => {
-    return history?.state[key] || initialValue
+    return history.state?.[key] || initialValue
   })
   const setCurrentWrapper = (value) => {
     setCurrent(value)
