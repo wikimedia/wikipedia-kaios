fad2227f86fa1ca303cb18cd0e287e4ce78692eb
Eduardo <medina.eag@gmail.com>
2020-05-06 22:17:00 -0400

    Introduce ConsentPopup component

--- src/utils/usageReportConsent.js
+++ src/utils/usageReportConsent.js
@@ -1,9 +1,18 @@
 const KEY = 'usage-data-consent'
+const confirmKey = 'usage-data-consent-confirmed-once'
 
 export const getConsentStatus = () => {
   return localStorage.getItem(KEY)
 }
 
+export const getConsentConfirmationStatus = () => {
+  return !!localStorage.getItem(confirmKey)
+}
+
 export const setConsentStatus = (consent) => {
   localStorage.setItem(KEY, consent)
+}
+
+export const setConsentConfirmationStatus = (confirmation) => {
+  localStorage.setItem(confirmKey, confirmation)
 }
