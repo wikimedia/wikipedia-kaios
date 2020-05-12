301ef8d1f52a2fb43b3d9c897d4775b9434ace7d
Eduardo <medina.eag@gmail.com>
2020-05-12 18:12:17 -0400

    Add placeholder and onKeyBackspace functionality

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,14 +1,24 @@
 import { h } from 'preact'
-import { useState } from 'preact/hooks'
-import { useI18n, useSoftkey } from 'hooks'
+import { useState, useRef, useEffect } from 'preact/hooks'
+import { useI18n, useSoftkey, useNavigation } from 'hooks'
 import { sendFeedback } from 'utils'
 
 export const Feedback = ({ close }) => {
+  const containerRef = useRef()
   const i18n = useI18n()
   const [message, setMessage] = useState()
+  const [, setNavigation, getCurrent] = useNavigation('Feedback', containerRef, containerRef, 'y')
 
   const onKeyRight = () => {
     sendFeedback(message)
+  }
+
+  const onKeyBackspace = () => {
+    if (message && getCurrent().type === 'TEXTAREA') {
+      setMessage(message.slice(0, -1))
+    } else {
+      close()
+    }
   }
 
   useSoftkey('Feedback', {
@@ -16,16 +26,20 @@ export const Feedback = ({ close }) => {
     onKeyRight,
     left: i18n('softkey-close'),
     onKeyLeft: close,
-    onKeyBackspace: close
+    onKeyBackspace
   }, [message])
 
+  useEffect(() => {
+    setNavigation(0)
+  }, [])
+
   return (
-    <div class='feedback'>
+    <div class='feedback' ref={containerRef}>
       <div class='header'>{i18n('feedback-header')}</div>
       <div class='body'>
         <div class='text-box'>
           <form>
-            <textarea value={message} onChange={e => setMessage(e.target.value)} />
+            <textarea value={message} placeholder={i18n('feedback-placeholder')} onChange={e => setMessage(e.target.value)} data-selectable />
           </form>
         </div>
         <div class='message-box'>
