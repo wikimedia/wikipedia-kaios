ac09be85cb89375e78dfc7a384cf8bc97bd9c4bc
Stephane Bisson <sbisson@wikimedia.org>
2020-05-14 11:38:02 -0400

    Fix tests

--- cypress/integration/special-cases.js
+++ cypress/integration/special-cases.js
@@ -32,8 +32,8 @@ describe('special cases tests', () => {
     })
     cy.rightArrow()
     cy.enter()
-    cy.get('.gallery.hasHeader>.header').should('be.visible')
-    cy.get('.gallery.hasHeader>.img>img').should('be.visible').should('have.attr', 'src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Tupolev_Tu-154B%2C_Tarom_AN0679876.jpg/640px-Tupolev_Tu-154B%2C_Tarom_AN0679876.jpg')
+    cy.get('.gallery-view.hasHeader>.header').should('be.visible')
+    cy.get('.gallery-view.hasHeader>.img>img').should('be.visible').should('have.attr', 'src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Tupolev_Tu-154B%2C_Tarom_AN0679876.jpg/640px-Tupolev_Tu-154B%2C_Tarom_AN0679876.jpg')
   })
 
   it('check goto quickfacts holly', () => {
