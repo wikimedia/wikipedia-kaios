c01c1f803864ce21bc165e5e32a2780d6e1007c6
Huei Tan <huei90@gmail.com>
2020-08-06 00:33:55 +0200

    Update ListView and Gallery RTL styling

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -13,8 +13,8 @@ export const ListView = ({ items = [], header, containerRef, empty }) => {
           items.length ? items.map(item => (
             <div class='item' dir={item.dir} data-selectable data-title={item.title} data-selected-key={item.title} key={item.title}>
               <div class='info'>
-                <div class='title' dangerouslySetInnerHTML={{ __html: item.displayTitle || item.title }} />
-                { item.description && <div class='description'>{item.description}</div> }
+                <bdi class='title' dangerouslySetInnerHTML={{ __html: item.displayTitle || item.title }} />
+                { item.description && <bdi class='description'>{item.description}</bdi> }
               </div>
               { item.imageUrl && <div class='img' style={{ backgroundImage: `url(${item.imageUrl})` }} /> }
               { item.link && <div class='link'><img src='/images/link.svg' /></div> }
