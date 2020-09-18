117f31faac830bd60fc75d88d2f647bb16675464
Jose Pita <dvpita@gmail.com>
2020-09-18 01:28:40 +0100

    disabling the test

--- cypress/integration/references-popup.js
+++ cypress/integration/references-popup.js
@@ -9,7 +9,7 @@ describe('references', () => {
     cy.navigateToHomePage()
   })
 
-  it('check preview for references opens', () => {
+  it.skip('check preview for references opens', () => {
     goToCatArticle()
     cy.downArrow()
     cy.rightArrow()
