9593c12377a1deea3588adeda204440950285897
Huei Tan <hueitan@users.noreply.github.com>
2020-12-04 16:11:45 +0100

    T267290 For Jio users show Indian languages on top inside the article menu (#284)
    * temp
    
    * Sort langlinks list follow of the priority language list (FIFO)

--- src/api/getLanglinks.js
+++ src/api/getLanglinks.js
@@ -1,6 +1,7 @@
 import {
   buildMwApiUrl, cachedFetch,
-  isSupportedForReading, getDirection
+  isSupportedForReading, getDirection,
+  prioritizedList
 } from 'utils'
 
 export const getLanglinks = (lang, title) => {
@@ -14,15 +15,36 @@ export const getLanglinks = (lang, title) => {
   const url = buildMwApiUrl(lang, params)
   return cachedFetch(url, response => {
     const { pages } = response.query
-    return pages[0].langlinks
-      .filter(item => isSupportedForReading(item.lang))
-      .map(item => ({
-        title: item.autonym,
-        langname: item.langname,
-        lang: item.lang,
-        description: item.title,
-        dir: getDirection(item.lang)
+    const langlinks = pages[0].langlinks
+    const prioritizedLanguages = langlinks
+      .filter(item => {
+        return prioritizedList.indexOf(item.lang) !== -1 && isSupportedForReading(item.lang)
       })
-      )
+      .map(item => {
+        return {
+          title: item.autonym,
+          langname: item.langname,
+          lang: item.lang,
+          description: item.title,
+          dir: getDirection(item.lang)
+        }
+      })
+    const allLanguages = prioritizedLanguages.concat(
+      langlinks
+        .filter(item => {
+          return isSupportedForReading(item.lang) && prioritizedList.indexOf(item.lang) === -1
+        })
+        .map(item => {
+          return {
+            title: item.autonym,
+            langname: item.langname,
+            lang: item.lang,
+            description: item.title,
+            dir: getDirection(item.lang)
+          }
+        })
+    )
+
+    return allLanguages
   })
 }
