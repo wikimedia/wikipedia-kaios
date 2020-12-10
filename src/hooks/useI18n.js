77f3416c3ed21f8c894b8669f98603fa387ca341
Stephane Bisson <sbisson@wikimedia.org>
2020-12-10 11:05:18 -0500

    Handle message parsing errors

--- src/hooks/useI18n.js
+++ src/hooks/useI18n.js
@@ -1,15 +1,35 @@
 import { useContext } from 'preact/hooks'
 import { I18nContext } from 'contexts'
-import { getAppLanguage } from 'utils'
+import { getAppLanguage, sendErrorLog } from 'utils'
 
 export const useI18n = (lang = getAppLanguage()) => {
   const banana = useContext(I18nContext)
 
-  return (key, ...args) => {
+  const i18n = (lang, key, ...args) => {
     if (lang !== banana.locale) {
       banana.setLocale(lang)
     }
 
     return banana.i18n(key, ...args)
+  }
+
+  return (key, ...args) => {
+    try {
+      // Try it in the requested language
+      return i18n(lang, key, ...args)
+    } catch (e) {
+      sendErrorLog(e)
+    }
+    try {
+      if (lang !== 'en') {
+        // If it failed and it was not English,
+        // Try it in English
+        return i18n('en', key, ...args)
+      }
+    } catch (e) {
+      sendErrorLog(e)
+    }
+    // Give up, return the key
+    return key
   }
 }
