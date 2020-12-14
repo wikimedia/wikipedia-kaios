2fe61c98863991a333ccaf68271cf9b4893234cb
Jose Pita <dvpita@gmail.com>
2020-12-14 23:26:22 +0000

    try

--- cypress/integration/references-popup.js
+++ cypress/integration/references-popup.js
@@ -11,10 +11,15 @@ describe('references', () => {
 
   it('check preview for references opens', () => {
     goToCatArticle()
+    cy.screenshot()
     cy.downArrow()
+    cy.screenshot()
     cy.rightArrow()
+    cy.screenshot()
     cy.enter()
+    cy.screenshot()
     cy.get('.ref-title').should('be.visible').should('contain.text', 'Reference [10]')
+    cy.screenshot()
   })
 })
 
