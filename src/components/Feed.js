7a7d415b8adba4e7302ad915bd752a535c04c749
Eduardo <medina.eag@gmail.com>
2021-02-16 12:22:38 -0400

    Reuse ListView instead of ListFeed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,7 @@
 import { h } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
 import { isTrendingArticlesGroup, isCuratedTopicsGroup } from 'utils'
+import { ListView } from 'components'
 import { useI18n } from 'hooks'
 import { getTrendingArticles } from 'api'
 
@@ -17,28 +18,8 @@ export const Feed = ({ isExpanded, lang, containerRef }) => {
   return (
     <div class={`feed ${isExpanded ? 'expanded' : 'collapsed'}`}>
       {!isExpanded && <div class='cue' />}
-      <div class='header'>{i18n('feed-header')}</div>
-      {isTrendingArticlesGroup() && <ListFeed items={trendingArticles} containerRef={containerRef} isExpanded={isExpanded} />}
+      {isTrendingArticlesGroup() && <ListView items={trendingArticles} header={i18n('feed-header')} containerRef={containerRef} />}
       {isCuratedTopicsGroup() && <TopicFeed />}
-    </div>
-  )
-}
-
-const ListFeed = ({ items, containerRef, isExpanded }) => {
-  // TODO: dir={item.dir} ?
-  return (
-    <div class={`trending-list ${isExpanded ? 'expanded' : 'collapsed'}`} ref={containerRef}>
-      {
-        items.length ? items.map(item => (
-          <div class='item ' data-selectable data-title={item.title} data-selected-key={item.title} key={item.title}>
-            <div class='info'>
-              <bdi class='title' dangerouslySetInnerHTML={{ __html: item.displayTitle || item.title }} />
-              { item.description && <bdi class='description' dangerouslySetInnerHTML={{ __html: item.description }} /> }
-            </div>
-            { item.thumb && <div class='img' style={{ backgroundImage: `url(${item.thumb})` }} /> }
-          </div>
-        )) : null
-      }
     </div>
   )
 }
