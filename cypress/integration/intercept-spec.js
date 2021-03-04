24e8be69b68c6ede2e5340fdfe8e721f787c6143
Gabriel Pita <dvpita@gmail.com>
2021-03-04 19:54:00 +0000

    fix eslint

--- cypress/integration/intercept-spec.js
+++ cypress/integration/intercept-spec.js
@@ -1,11 +1,9 @@
 /// <reference types="Cypress" />
 
 import { SearchPage } from '../page-objects/search-page'
-import { ArticlePage } from '../page-objects/article-page'
-import * as enJson from '../../i18n/en.json'
 
 const searchPage = new SearchPage()
-const articlePage = new ArticlePage()
+
 describe('Intercept search', () => {
   beforeEach(() => {
     cy.navigateToHomePage()
@@ -13,9 +11,8 @@ describe('Intercept search', () => {
 
   it('search should show results', () => {
     searchPage.search('catt')
-    cy.intercept('/api.php', {fixture: 'catt-search.json'})
+    cy.intercept('/api.php', { fixture: 'catt-search.json' })
     searchPage.results().first()
       .should('exist')
   })
-  
 })
