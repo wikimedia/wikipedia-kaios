031e500187e18d7f332a1f036b74f1be5d102cbc
Eduardo <medina.eag@gmail.com>
2021-03-04 09:54:12 -0400

    Add feed loading UI

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,13 +6,18 @@ import { getTrendingArticles } from 'api'
 
 export const Feed = ({ lang, isExpanded, setIsExpanded, lastIndex, setNavigation, containerRef }) => {
   const [trendingArticles, setTrendingArticles] = useState([])
+  const [loading, setLoading] = useState(true)
   const i18n = useI18n()
 
   useEffect(() => {
+    setLoading(true)
     const [request, abort] = getTrendingArticles(lang)
-    request.then(setTrendingArticles)
+    request.then(articles => {
+      setLoading(false)
+      setTrendingArticles(articles)
+    })
     return abort
-  })
+  }, [])
 
   useEffect(() => {
     if (lastIndex) {
@@ -21,10 +26,48 @@ export const Feed = ({ lang, isExpanded, setIsExpanded, lastIndex, setNavigation
     }
   }, [trendingArticles])
 
+  useEffect(() => {
+    if (isExpanded && loading) {
+      setNavigation(1)
+    }
+  }, [loading, isExpanded])
+
   return (
     <div class={`feed ${isExpanded ? 'expanded' : 'collapsed'}`}>
       {!isExpanded && <div class='cue' />}
-      <ListView items={trendingArticles} header={i18n('feed-header')} containerRef={containerRef} />
+      {!loading && <ListView items={trendingArticles} header={i18n('feed-header')} containerRef={containerRef} />}
+      {loading && <Loading isExpanded={isExpanded} />}
+    </div>
+  )
+}
+
+const Loading = ({ isExpanded }) => {
+  const loadingExpanded = () => {
+    const loadingItem = (selectable = false) => {
+      return (
+        <div class='item' data-selectable={selectable}>
+          <div class='bars'>
+            <div class='bar' />
+            <div class='bar' />
+          </div>
+          <div class='rectangle' />
+        </div>
+      )
+    }
+
+    return (
+      <div class='expanded'>
+        {loadingItem(true)}
+        {loadingItem()}
+        {loadingItem()}
+      </div>
+    )
+  }
+
+  return (
+    <div class='loading'>
+      {!isExpanded && <div class='collapsed' />}
+      {isExpanded && loadingExpanded()}
     </div>
   )
 }
