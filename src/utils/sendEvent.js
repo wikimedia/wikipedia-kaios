f07f4e20c21654b81dd745ccead10e696b805b59
Stephane Bisson <sbisson@wikimedia.org>
2021-03-17 12:32:36 -0400

    Don't wrap object in array

--- src/utils/sendEvent.js
+++ src/utils/sendEvent.js
@@ -3,10 +3,10 @@ import { isProd } from 'utils'
 const intakeUrl = 'https://intake-analytics.wikimedia.org/v1/events' + (isProd() ? '?hasty=true' : '')
 
 export const sendEvent = ($schema, stream, event) => {
-  const body = JSON.stringify([{
+  const body = JSON.stringify({
     $schema,
     meta: { stream },
     event
-  }])
+  })
   navigator.sendBeacon(intakeUrl, body)
 }
