135cfd189bc1a0b24f7920455c3e614d7f5228fe
Huei Tan <huei90@gmail.com>
2021-03-19 10:18:14 +0100

    enable image and fix test spec

--- cypress/integration/special-cases.js
+++ cypress/integration/special-cases.js
@@ -24,7 +24,6 @@ describe('special cases tests', () => {
     cy.navigateToPageWithoutOnboarding('article/pl/Tupolew_Tu-154/Użytkownicy[24]')
     cy.wait(500)
     articlePage.title().should('have.text', 'Użytkownicy[24]')
-    cy.rightArrow()
     cy.enter()
     cy.get('.gallery-view>.img>img').should('be.visible').should('have.attr', 'src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Tupolev_Tu-154B%2C_Tarom_AN0679876.jpg/640px-Tupolev_Tu-154B%2C_Tarom_AN0679876.jpg')
   })
