2ef851c61846f11226fc8dce3db1395daaee0d4b
Stephane Bisson <sbisson@wikimedia.org>
2021-07-19 14:15:47 -0400

    Print only the relevant memory stats

--- scripts/stats_header.sh
+++ scripts/stats_header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# USS and PSS are the important columns for memory usage analysis
-echo 'TIME NAME PID PPID CPU(s) NICE USS PSS RSS SWAP VSIZE ADJ SCORE_ADJ USER'
+echo 'TIME USS PSS'
