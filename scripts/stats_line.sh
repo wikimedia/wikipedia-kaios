0013ad479a33602ff6777aef6cf3d2eaffa743dc
Stephane Bisson <stephane.c.bisson@gmail.com>
2021-07-20 07:22:41 -0400

    Print only the relevant memory stats (#373)
    Co-authored-by: Stephane Bisson <sbisson@wikimedia.org>

--- scripts/stats_line.sh
+++ scripts/stats_line.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DATE=`date "+%H:%M:%S"`
-LINE=`adb shell b2g-info | grep Wikipedia | sed -E 's/ +/ /g' | sed -E 's/^ //'`
+LINE=`adb shell b2g-info | grep Wikipedia | sed -E 's/ +/ /g' | sed -E 's/^ //' | cut -d' ' -f6,7`
 echo $DATE $LINE
 $0
